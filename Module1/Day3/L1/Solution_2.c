2a34e6b62d6f53e0551f56a8a0524b15ee99f22f
KarthikBhaskaran22 <karthikbhaskaran22@gmail.com>
2023-06-15 19:54:31 +0530

    Adding Module1 - Day3 - L1 Solutions

---
+++ Module1/Day3/L1/Solution_2.c
@@ -0,0 +1,48 @@
+/*
+Task 2
+Prints Bits:
+Scan a 32bit integer and prints its bits.
+
+Concepts to be used.
+- Loops
+- Bitwise Operators
+
+*/
+
+#include <stdio.h>
+
+void bits(unsigned int num)
+{
+   int i;
+   int number = sizeof(num) * 8;
+
+   for (i = number - 1; i >= 0; i--)
+   {
+      unsigned int mask = 1u << i;
+
+      if (num & mask)
+      {
+         printf("1");
+      }
+      else
+      {
+         printf("0");
+      }
+
+      if (i % 8 == 0)
+      {
+         printf(" ");
+      }
+   }
+
+   printf("\n");
+}
+
+int main()
+{
+   unsigned int num;
+   printf("Enter a 32-bit integer: ");
+   scanf("%u", &num);
+   bits(num);
+   return 0;
+}
