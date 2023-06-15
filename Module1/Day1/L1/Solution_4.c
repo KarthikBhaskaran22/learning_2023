c71b1c813ce035e4e142229e72d2257301c3385a
KarthikBhaskaran22 <karthikbhaskaran22@gmail.com>
2023-06-15 17:38:12 +0530

    Adding Module1 - Day1 - L1 Solutions

---
+++ Module1/Day1/L1/Solution_4.c
@@ -0,0 +1,68 @@
+/*
+Task 4
+Write a simple calculator program which should accept inputs in the same order.
+    - Operand 1
+    - Operator
+    - Operand 2
+
+The order of scan should be same, for example, you should first scan in the following order.
+printf("Enter Number1: );
+Scan the first number form the user.
+printf("Enter the operator: );
+Scan the operator from the user.
+printf("Enter Number2: );
+Scan the second number form the user.
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+   float num1, num2, result;
+   char operator;
+
+   printf("Enter the First number: ");
+   scanf("%f", &num1);
+
+   printf("Enter the Operator (+, -, *, /): ");
+   scanf(" %c", &operator);
+
+   printf("Enter the Second number: ");
+   scanf("%f", &num2);
+
+   switch (operator)
+   {
+   case '+':
+      result = num1 + num2;
+      printf("Result: %.2f\n", result);
+      break;
+
+   case '-':
+      result = num1 - num2;
+      printf("Result: %.2f\n", result);
+      break;
+
+   case '*':
+      result = num1 * num2;
+      printf("Result: %.2f\n", result);
+      break;
+
+   case '/':
+      if (num2 != 0)
+      {
+         result = num1 / num2;
+         printf("Result: %.2f\n", result);
+      }
+      else
+      {
+         printf("Error: Division by zero is not allowed.\n");
+      }
+      break;
+      
+   default:
+      printf("Error: Invalid operator.\n");
+   }
+
+   return 0;
+}
