0e550d21f63f9d470c89170809987d28ef8eae0b
KarthikBhaskaran22 <karthikbhaskaran22@gmail.com>
2023-06-15 17:41:48 +0530

    Adding Module1 - Day1 - L2 Solutions

---
+++ Module1/Day1/L2/Solution_4.c
@@ -0,0 +1,58 @@
+/*
+Task 4
+Write a function to accept an integer (32 bits) and operation type to do the following and return the result to main function.
+    - Set 2 bits from nth bit position, if operation type is 1
+    - Clear 3 bits from nth bit position, if operation type is 2
+    - Toggle MSB, if operation type is 3
+   You can consider the below function prototype,
+    - bit_operations(int num, int oper_type, int pos);
+
+*/
+
+#include <stdio.h>
+
+int bit_operations(int num, int oper_type, int pos)
+{
+   if (oper_type == 1)
+   {
+      // Set 2 bits
+      int mask = (1 << 2) - 1;
+      mask = mask << pos;
+      return num | mask;
+   }
+   else if (oper_type == 2)
+   {
+      // Clear 3 bits
+      int mask = (1 << 3) - 1;
+      mask = mask << pos;
+      mask = ~mask;
+      return num & mask;
+   }
+   else if (oper_type == 3)
+   {
+      // Toggle MSB
+      int mask = 1 << 31; 
+      return num ^ mask;
+   }
+   else
+   {
+      printf("Invalid operation type\n");
+      return num;
+   }
+}
+
+int main()
+{
+   int num, oper_type, pos;
+   printf("Enter the number: ");
+   scanf("%d", &num);
+   printf("Enter the operation type (1, 2, or 3): ");
+   scanf("%d", &oper_type);
+   printf("Enter the position: ");
+   scanf("%d", &pos);
+
+   int result = bit_operations(num, oper_type, pos);
+   printf("Result: %d\n", result);
+
+   return 0;
+}
