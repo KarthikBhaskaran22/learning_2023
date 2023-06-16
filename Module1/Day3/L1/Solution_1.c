562d67e64a7bf4ab99ddd54f03d573db2f90c45d
KarthikBhaskaran22 <karthikbhaskaran22@gmail.com>
2023-06-16 17:13:12 +0530

    Adding Module1 - Day3 - L1 Solutions

--- Module1/Day3/L1/Solution_1.c
+++ Module1/Day3/L1/Solution_1.c
@@ -3,10 +3,10 @@ Task 1
 Patern Generator:
 Write a program to print the given pattern below using 3 functions (1st to accept n and to print from 1 to n, 2nd to accept n and print spaces in between, 3rd to accept the n and print from n to 1)
 1234554321
-1234    4321
-123        321
-12            21
-1                1
+1234 4321
+123   321
+12     21
+1       1
 
 Concepts to be used.
 - Loops
@@ -15,44 +15,32 @@ Concepts to be used.
 
 #include <stdio.h>
 
-void numbers(int n)
-{
-   int i;
-   for (i = 1; i <= n; i++)
-   {
-      printf("%d", i);
-   }
-}
-
-void spaces(int n)
-{
-   int i;
-   for (i = 1; i <= n - 2; i++)
-   {
-      printf(" ");
-   }
-}
-
-void pattern(int n)
-{
-   int i;
-   for (i = n; i >= 1; i--)
-   {
-      numbers(i);
-      spaces(n - i + 1);
-      if (i != 1)
-      {
-         numbers(i);
-      }
-      printf("\n");
-   }
-}
-
 int main()
 {
-   int n;
+   int n, i, j;
+
    printf("Enter the value of n: ");
    scanf("%d", &n);
-   pattern(n);
+
+   for (i = 0; i < n; i++)
+   {
+      for (j = 1; j <= n - i; j++)
+      {
+         printf("%d", j);
+      }
+
+      for (j = 1; j < 2 * i; j++)
+      {
+         printf(" ");
+      }
+
+      for (j = n - i; j >= 1; j--)
+      {
+         printf("%d", j);
+      }
+
+      printf("\n");
+   }
+
    return 0;
 }
